882bc04e08d48cd2d876501c8bd746e92ae2a125
Franck DEMEYER <franck.demeyer@free.fr>
2022-11-25 14:03:09 +0100

    [Quarkus][Workshop][25/11/22] Fault Tolerance.

--- src/main/java/com/zenika/quarkus/workshop/restclients/WeatherClient.java
+++ src/main/java/com/zenika/quarkus/workshop/restclients/WeatherClient.java
@@ -1,6 +1,9 @@
 package com.zenika.quarkus.workshop.restclients;
 
 import com.zenika.quarkus.workshop.entities.Weather2;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.ws.rs.GET;
@@ -11,6 +14,9 @@ import java.util.List;
 public interface WeatherClient {
 
     @GET
+    @Retry
+    @CircuitBreaker
+    @Timeout(500L)
     Weather2 getWeather(@QueryParam("daily") List<String> wantedInfo,
                         @QueryParam("longitude") double longitude,
                         @QueryParam("latitude") double latitude,
